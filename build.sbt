7f76b106fa0f9752bd44b090511d7682b6b5d520
sungbinyoon <gamsif90@gmail.com>
2018-08-07 23:19:44 +0900

    scala wordCount
    - param : `local targetPath destinationPath
    `

--- build.sbt
+++ build.sbt
@@ -1,5 +1,22 @@
 name := "spark_example"
 
-version := "0.1"
-
 scalaVersion := "2.11.8"
+
+val sparkVer = "2.2.0"
+
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVer,
+  "org.apache.spark" %% "spark-streaming" % sparkVer,
+  "org.apache.spark" %% "spark-sql" % sparkVer,
+  "org.apache.spark" %% "spark-mllib" % sparkVer,
+  "org.apache.spark" %% "spark-repl" % sparkVer,
+  "org.apache.spark" %% "spark-yarn" % sparkVer,
+  "org.apache.spark" %% "spark-hive" % sparkVer
+)
+
+javacOptions ++= Seq("-source", "1.8", "-target", "1.8")
+
+retrieveManaged := true
+
+publishMavenStyle := true
---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,48 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+// 실행 파라미터
+// example local ${TARGET_FILE} ${DESTINATION_DIR}
+// local -> thread count
+// local : default = 1 ( local[1] )
+// local[2] : thread = 2 ( par-xx file 2개 생김)
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+
+    require(args.length == 3, "Usage: WordCount <Master> <Input> <Output>")
+    println(args.mkString(", "))
+
+    val sc = getSparkContext("WordCount", args(0))
+
+    val inputRDD = getInputRDD(sc, args(1))
+
+    val resultRDD = process(inputRDD)
+
+    handleResult(resultRDD, args(2))
+  }
+
+  def getSparkContext(appName: String, master: String): SparkContext = {
+    // create config
+    val conf = new SparkConf().setAppName(appName).setMaster(master)
+    // create spark context
+    new SparkContext(conf)
+  }
+
+  def getInputRDD(sc: SparkContext, input: String): RDD[String] = {
+    // make RDD object by file read
+    sc.textFile(input)
+  }
+
+  def process(inputRDD: RDD[String]): RDD[(String, Int)] = {
+    // tokenize by space
+    val words = inputRDD.flatMap(str => str.split(" "))
+    val wcPair = words.map((_, 1))
+    // reduce
+    wcPair.reduceByKey(_ + _)
+  }
+
+  def handleResult(resultRDD: RDD[(String, Int)], output: String): Unit = {
+    resultRDD.saveAsTextFile(output)
+  }
+}
